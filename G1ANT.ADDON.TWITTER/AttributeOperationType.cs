02c3ec065b955ebdf095b23d33423e0551eced79
SANSKAR GUPTA <54939968+sanskar1419@users.noreply.github.com>
2020-08-29 19:23:24 +0530

    Add files via upload

---
+++ G1ANT.ADDON.TWITTER/AttributeOperationType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Twit
+{
+    public enum AttributeOperationType
+    {
+        PreferAttribute,
+        ForceAttribute,
+        ForceProperty,
+        Default = PreferAttribute
+    }
+
+    public static class AttributeOperationTypeParser
+    {
+        public static AttributeOperationType Parse(string operationTypeName)
+        {
+            if (string.IsNullOrEmpty(operationTypeName))
+                return AttributeOperationType.Default;
+
+            return (AttributeOperationType)Enum.Parse(typeof(AttributeOperationType), operationTypeName, true);
+        }
+    }
+}
---
+++ G1ANT.ADDON.TWITTER/ElementSearchBy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Twit
+{
+    public enum ElementSearchBy
+    {
+        Id = 0,
+        Class = 1,
+        CssSelector = 2,
+        Tag = 3,
+        Xpath = 4,
+        Name = 5,
+        Query = 6,
+        Jquery = 7
+    }
+}
